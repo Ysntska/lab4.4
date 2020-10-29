6e40e51793fd72915a729a230e77be7a0ff84d4c
Ysntska <yasynetska27@gmail.com>
2020-10-29 13:25:02 +0200

    first

---
+++ 4.4/4.4.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double x, xp, xk, dx, y, R;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << "R = "; cin >> R;
+	cout << fixed;
+	cout << "----------------------" << endl;
+	cout << "|" << setw(7) << "x" << " |"
+		<< setw(10) << "y" << " |" << endl;
+	cout << "----------------------" << endl;
+	x = xp;
+	while (x <= xk)
+	{
+		if (x <= -2)
+			y = x + 3;
+		else
+			if (-2 < x && x <= 4)
+				y = (-(1 + R) / 6) * x;
+			else
+				if (4 < x && x <= 8 - R)
+					y = -R;
+				else
+					if (8 - R < x && x <= 8 + R)
+						y = sqrt(R * R - (x - 8) * (x - 8)) - R;
+					else
+						y = -R;
+
+		cout << "|" << setw(7) << setprecision(2) << x
+			<< " |" << setw(10) << setprecision(3) << y
+			<< " |" << endl;
+		x += dx;
+	}
+	cout << "----------------------" << endl;
+	return 0;
+}
+
